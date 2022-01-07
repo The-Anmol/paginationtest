f4d5ee06da1e5bef0aa2a4e7dc8fdc6af524fc90
Anmol <88301047+Anmolkilla@users.noreply.github.com>
2022-01-07 10:21:41 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,15 +1,25 @@
+const pgnButtons = document.querySelectorAll(".pgn__num");
 let articleTag = document.getElementsByTagName("article");
 let totalArticleTag = articleTag.length ;
-console.log(totalArticleTag);
+const maxPosts = 5 ; 
 
-
-const pgnButtons = document.querySelectorAll(".pgn__num");
+for (var i = 0 ; i <maxPosts ; i++) {
+        articleTag[i].classList.add("show")
+    }
+    
 for ( clickedButton of pgnButtons ) {
     clickedButton.addEventListener("click", function(e){
-        
         let text = e.target.textContent ;
-        console.log(text);
-        console.log(articleTag[text]);
-        articleTag[text].classList.add("change");
+        if (clickedButton.clicked == true ) {
+            for (var i = text*maxPosts-maxPosts ; i <text*maxPosts-1 ; i++) {
+            articleTag[i].classList.add("show")
+            }
+        }
     })
+}
+function clicked(){
+    let text = this.textContent ;
+    for (var i = text*maxPosts-maxPosts ; i <text*maxPosts-1 ; i++) {
+        articleTag[i].classList.add("show")
+    }
 }
